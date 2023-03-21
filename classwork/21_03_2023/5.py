8ac0d89f24db4f293b3d2f37092a55574afe53f2
Propolisss <artem89296145449@gmail.com>
2023-03-21 15:01:08 +0300

    classwork 21.03.2023

--- classwork/21_03_2023/5.py
+++ classwork/21_03_2023/5.py
@@ -1,42 +1,18 @@
 file = open('26 (1).txt')
 
-def find_minn(n_):
-    for i in range(n):
-        if not(flags[i]) and med[i] <= n_:
-            return i
-    return 'aaa'
-
-def find_minn2():
-    for i in range(n):
-        if not(flags[i]):
-            return i
-    return 'aaa'
-
-
 n = int(file.readline())
 summ = 100_000
 nums = sorted([int(i) for i in file])
-flags = [0] * n
-med = [i // 2 for i in nums]
-count = 0
 
-curr_summ = 0
-red_ind = 0
-red_summ = 0
 red_arr = []
+maxx = float('-inf')
+maxx2 = float('-inf')
 
 for i in range(len(nums)):
-    red_summ += nums[i]
     red_arr.append(nums[i])
-    if red_summ > summ:
-        red_ind = i
-        red_summ -= nums[i]
-        del red_arr[-1]
-        break
-med = med[red_ind:]
-maxx = float('-inf')
-count = 0
-
-while True:
-    count_red = len(red_arr) // 6
-    sale = sum(red_arr[-count_red:]) // 2
+    sale = len(red_arr) // 6
+    if (sum(red_arr[:-sale]) + (sum(red_arr[-sale:]) // 2)) <= 100_000:
+        maxx = max(maxx, len(red_arr))
+        maxx2 = 100_000 - (sum(red_arr[:-sale]) + (sum(red_arr[-sale:]) // 2))
+        print(maxx, maxx2)
+print(maxx, maxx2)
